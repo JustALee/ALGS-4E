05297334a8d669c1304650cc5756102ced898f94
JustALee <imJustALee@hotmail.com>
2019-08-16 09:43:54 +0800

    Add 3 classes

---
+++ src/BinarySearch.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int[] whitelist = in.readAllInts();
+        Arrays.sort(whitelist);
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (rank(key, whitelist) < 0) {
+                StdOut.println(key);
+            }
+        }
+    }
+    public static int rank(int key, int[] a) {
+        int lo = 0;
+        int hi = a.length-1;
+        while (lo<=hi) {
+            int mid = lo+(hi-lo)/2;
+            if (a[mid] > key) hi = mid-1;
+            else if (a[mid] < key) lo = mid+1;
+            else return mid;
+        }
+        return -1;
+    }
+}
