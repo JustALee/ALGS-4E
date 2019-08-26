beafe95e4bf33e75115f97d10719ccb69805481c
JustALee <imJustALee@hotmail.com>
2019-08-26 15:53:03 +0800

    Add FlipsMax.java

---
+++ src/chapter1/section2/FlipsMax.java
@@ -0,0 +1,34 @@
+package chapter1.section2;
+
+import edu.princeton.cs.algs4.Counter;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class FlipsMax {
+    public static void main(String[] args) {
+        int T = StdIn.readInt();
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+        for (int t=0; t<T; t++) {
+            if (StdRandom.bernoulli(0.5)) {
+                heads.increment();
+            } else {
+                tails.increment();
+            }
+        }
+        if (heads.tally() == tails.tally()) {
+            StdOut.println("Tie");
+        } else {
+            StdOut.println(max(heads, tails) + " wins");
+        }
+    }
+
+    public static Counter max(Counter x, Counter y) {
+        if (x.tally() > y.tally()) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+}
