e252f2bf3a009c9697d9ae595e9ea152c345629a
JustALee <imJustALee@hotmail.com>
2019-08-28 14:07:30 +0800

    Add Accumulator.java

---
+++ src/chapter1/section2/Accumulator.java
@@ -0,0 +1,35 @@
+package chapter1.section2;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import javax.swing.*;
+
+public class Accumulator {
+    private int N;
+    private double total;
+
+    public void addDataValue(double val) {
+        N++;
+        total += val;
+    }
+
+    public double mean() {
+        return total/N;
+    }
+
+    public String toString() {
+        return "Mean (" + N + " values): "
+                + String.format("%7.5f", mean());
+    }
+
+    public static void main(String[] args) {
+        int T = StdIn.readInt();
+        Accumulator a = new Accumulator();
+        for (int t=0; t<T; t++) {
+            a.addDataValue(StdRandom.uniform());
+        }
+        StdOut.println(a);
+    }
+}
