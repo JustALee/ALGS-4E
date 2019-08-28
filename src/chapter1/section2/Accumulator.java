033c97f3e9eb3883c19d10b0b640ae43245c2222
JustALee <imJustALee@hotmail.com>
2019-08-28 14:09:56 +0800

    Remove unused import statement

--- src/chapter1/section2/Accumulator.java
+++ src/chapter1/section2/Accumulator.java
@@ -4,8 +4,6 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
-import javax.swing.*;
-
 public class Accumulator {
     private int N;
     private double total;
