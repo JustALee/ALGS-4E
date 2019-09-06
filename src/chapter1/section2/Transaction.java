ed3c8c8b5e85980304834b535d18566e3a829bca
JustALee <imJustALee@hotmail.com>
2019-09-06 18:49:46 +0800

    Add Transaction.java(Exercise 1.2.13)

---
+++ src/chapter1/section2/Transaction.java
@@ -0,0 +1,29 @@
+package chapter1.section2;
+
+public class Transaction {
+    private final String who;
+    private final Date when;
+    private final double amount;
+
+    public Transaction(String who, Date when, double amount) {
+        this.who = who;
+        this.when = when;
+        this.amount = amount;
+    }
+
+    public String who() {
+        return who;
+    }
+
+    public Date when() {
+        return when;
+    }
+
+    public double amount() {
+        return amount;
+    }
+
+    public String toString() {
+        return who() + " made a $" + amount() + " transaction on " + when();
+    }
+}
