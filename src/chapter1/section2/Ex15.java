66788c4bc460ff84e5fe95d16e60c19ab015c201
JustALee <imJustALee@hotmail.com>
2019-09-06 19:26:46 +0800

    Implement readInts method of In(Exercise 1.2.15)

---
+++ src/chapter1/section2/Ex15.java
@@ -0,0 +1,16 @@
+package chapter1.section2;
+
+import edu.princeton.cs.algs4.In;
+
+public class Ex15 {
+    public static int[] readInts(String name) {
+        In in = new In(name);
+        String input = in.readAll();
+        String[] words = input.split("\\s+");
+        int[] ints = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            ints[i] = Integer.parseInt(words[i]);
+        }
+        return ints;
+    }
+}
