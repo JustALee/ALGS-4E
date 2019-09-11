b65a2cd1c4586c06d6f8ee299a6e5d8129cc0e92
JustALee <imJustALee@hotmail.com>
2019-09-11 18:46:37 +0800

    Add ResizingArrayStack.java(Algorithm 1.1)

---
+++ src/chapter1/section3/ResizingArrayStack.java
@@ -0,0 +1,62 @@
+package chapter1.section3;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
+public class ResizingArrayStack<Item> {
+    private Item[] a = (Item[]) new Object[1];
+    private int N = 0;
+
+    public ResizingArrayStack(int cap) {
+        a = (Item[]) new Object[cap];
+    }
+
+    public Boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void resize(int max) {
+        Item[] temp = (Item[]) new Object[max];
+        System.arraycopy(a, 0, temp, 0, a.length);
+        a = temp;
+    }
+
+    public void push(Item item) {
+        if (N == a.length) {
+            resize(2*a.length);
+        }
+        a[N++] = item;
+    }
+
+    public Item pop() {
+        Item item = a[--N];
+        a[N] = null;
+        if (N > 0 && N == a.length/4)
+            resize(a.length/2);
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = N;
+
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        public Item next() {
+            return a[--i];
+        }
+
+        public void remove() {}
+    }
+}
