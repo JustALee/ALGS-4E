553fa5124ab4b1566afa36a7c401301129b90929
JustALee <imJustALee@hotmail.com>
2019-09-13 15:05:16 +0800

    Add Bag.java(Algorithm 1.4)

---
+++ src/chapter1/section3/Bag.java
@@ -0,0 +1,49 @@
+package chapter1.section3;
+
+import java.util.Iterator;
+
+public class Bag<Item> {
+    private Node first;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void add(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+
+        public void remove() {}
+    }
+}
