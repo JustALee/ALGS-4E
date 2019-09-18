3ef47f844cc21e09b630a49f22645666128d17ec
JustALee <imJustALee@hotmail.com>
2019-09-18 18:45:13 +0800

    Add comment to Ex09.java

--- src/chapter1/section3/Ex09.java
+++ src/chapter1/section3/Ex09.java
@@ -5,6 +5,13 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Ex09 {
     public static void main(String[] args) {
+        /*
+        As you know, an expression is made of one operator and two operands,
+        what we need to do is split the whole input as multi subexpressions,
+        every time we meet a ), we pop 1 operator and 2 operands,
+        insert the missing ( on the left, to build a new string and see it as a operand.
+        repeat until you've parsed all the inputs.
+         */
         Stack<String> ops = new Stack<>();
         Stack<String> vals = new Stack<>();
         while (!StdIn.isEmpty()) {
